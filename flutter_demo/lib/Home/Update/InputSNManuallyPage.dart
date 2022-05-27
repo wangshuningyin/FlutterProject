5072b273a210bcea67ec51bb66f5481d53707550
NingSpeals <13072185708@13.com>
2022-05-27 14:15:11 +0800

    添加点击按钮

--- flutter_demo/lib/Home/Update/InputSNManuallyPage.dart
+++ flutter_demo/lib/Home/Update/InputSNManuallyPage.dart
@@ -1,5 +1,4 @@
 import 'dart:io';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
@@ -17,21 +16,182 @@ class _InputSNManuallyPageState extends State<InputSNManuallyPage> {
   TextEditingController thirdController = TextEditingController();
   FocusNode thirdFocusNode = FocusNode();
   String inputText = '';
-  RegExp englishRegExp = RegExp(r'(^[a-zA-Z]*$)');
+  String fristInputText = '';
+  String secondInputText = '';
+  String thridInputText = '';
+  String selectedManufactureStr = '';
+  String selectedModelStr = '';
+  Color btnBackgorondColor = Colors.grey.shade300;
+  Color btnTextColor = Colors.black;
   @override
   void initState() {
     super.initState();
     if (Platform.isIOS) {}
     firstController.addListener(() {
-      firstController.text.toUpperCase();
-      print("dfdfdgfdg${firstController.text}");
-      if (firstController.text.length > 7) {}
+      setState(() {
+        fristInputText = firstController.text;
+        if (fristInputText.isEmpty ||
+            secondInputText.isEmpty ||
+            thridInputText.isEmpty) {
+          btnBackgorondColor = Colors.grey.shade300;
+          btnTextColor = Colors.black;
+        } else {
+          btnBackgorondColor = Colors.red;
+          btnTextColor = Colors.white;
+        }
+      });
     });
+    secondController.addListener(() {
+      setState(() {
+        secondInputText = secondController.text;
+        if (fristInputText.isEmpty ||
+            secondInputText.isEmpty ||
+            thridInputText.isEmpty) {
+          btnBackgorondColor = Colors.grey.shade300;
+          btnTextColor = Colors.black;
+        } else {
+          btnBackgorondColor = Colors.red;
+          btnTextColor = Colors.white;
+        }
+      });
+    });
+    thirdController.addListener(() {
+      thridInputText = thirdController.text;
+      setState(() {
+        thridInputText = thirdController.text;
+        if (fristInputText.isEmpty ||
+            secondInputText.isEmpty ||
+            thridInputText.isEmpty) {
+          btnBackgorondColor = Colors.grey.shade300;
+          btnTextColor = Colors.black;
+        } else {
+          btnBackgorondColor = Colors.red;
+          btnTextColor = Colors.white;
+        }
+      });
+    });
+  }
+
+  Function()? _saveAction() {
+    print(thirdController.text);
+    if (fristInputText.isEmpty ||
+        secondInputText.isEmpty ||
+        thridInputText.isEmpty) {
+      return () {
+        return null;
+      };
+    }
+    return () {
+      // Navigator.pushNamed(
+      //   context,
+      //   Routes.modbusRtuPage,
+      //   arguments: widget.type,
+      // );
+      print("object");
+    };
+  }
+
+  Widget textEditingWidget(
+    String hintText,
+    double left,
+    double top,
+    TextEditingController controller,
+    FocusNode focusNode,
+    int limitLength,
+    double textLength,
+    FocusNode requestFocus,
+  ) {
+    return Container(
+      width: 70,
+      height: 30,
+      margin: EdgeInsets.only(left: left, top: top, right: 0),
+      child: TextField(
+        controller: controller,
+        focusNode: focusNode,
+        autofocus: false,
+        style: const TextStyle(
+          fontSize: 14,
+          color: Colors.black,
+          fontWeight: FontWeight.bold,
+        ),
+        decoration: InputDecoration(
+          border: InputBorder.none,
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: Colors.grey[350],
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        inputFormatters: <TextInputFormatter>[
+          UpperCaseTextFormatter(),
+          LengthLimitingTextInputFormatter(limitLength) //限制长度
+        ],
+        onChanged: (value) {
+          if (controller.text.length > textLength) {
+            inputText = controller.text.substring(0, limitLength);
+            controller.value = TextEditingValue(
+              text: inputText.toUpperCase(),
+              selection: TextSelection.collapsed(offset: inputText.length),
+            );
+            return;
+          }
+          if (controller.text.length >= limitLength) {
+            focusNode.unfocus();
+            FocusScope.of(context).requestFocus(requestFocus);
+          }
+        },
+      ),
+    );
+  }
+
+  Widget lineWidget(
+      double left, double top, Color color, double width, double height) {
+    return Container(
+      margin: EdgeInsets.only(left: left, top: top),
+      width: width,
+      height: height,
+      decoration: BoxDecoration(
+        color: color,
+        borderRadius: BorderRadius.circular(2),
+      ),
+    );
+  }
+
+  Widget saveWidget() {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.only(top: 60, bottom: 40),
+        width: 140,
+        height: 40,
+        child: ElevatedButton(
+          style: ButtonStyle(
+            elevation: MaterialStateProperty.all(0),
+            backgroundColor: MaterialStateProperty.all(btnBackgorondColor),
+            shape: MaterialStateProperty.all(
+              RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(35),
+              ),
+            ),
+          ),
+          child: Text(
+            "OK",
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w500,
+              color: btnTextColor,
+            ),
+          ),
+          onPressed: _saveAction(),
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.white,
       appBar: AppBar(
         title: const Text(
@@ -80,131 +240,20 @@ class _InputSNManuallyPageState extends State<InputSNManuallyPage> {
               margin: const EdgeInsets.only(left: 30, top: 5),
               child: Row(
                 children: [
-                  Container(
-                    width: 70,
-                    height: 30,
-                    margin: const EdgeInsets.only(left: 5, top: 10, right: 0),
-                    child: TextField(
-                      controller: firstController,
-                      focusNode: firstFocusNode,
-                      autofocus: false,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "TACW22",
-                        hintStyle: TextStyle(
-                          color: Colors.grey[350],
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      inputFormatters: <TextInputFormatter>[
-                        UpperCaseTextFormatter(),
-                        LengthLimitingTextInputFormatter(6) //限制长度
-                      ],
-                      onChanged: (value) {
-                        ///禁止联想输入
-                        if (firstController.text.length > 7) {
-                          inputText = firstController.text.substring(0, 6);
-                          firstController.value = TextEditingValue(
-                              text: inputText.toUpperCase(),
-                              selection: TextSelection.collapsed(
-                                  offset: inputText.length));
-                          return;
-                        }
-                      },
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 20, top: 5),
-                    width: 20,
-                    height: 1.5,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                  ),
-                  Container(
-                    width: 60,
-                    height: 30,
-                    margin: const EdgeInsets.only(left: 25, top: 10, right: 0),
-                    child: TextField(
-                      controller: secondController,
-                      focusNode: secondFocusNode,
-                      autofocus: false,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "40320",
-                        hintStyle: TextStyle(
-                          color: Colors.grey[350],
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      inputFormatters: <TextInputFormatter>[
-                        UpperCaseTextFormatter(),
-                        LengthLimitingTextInputFormatter(5) //限制长度
-                      ],
-                    ),
-                  ),
-                  Container(
-                    margin: const EdgeInsets.only(left: 20, top: 5),
-                    width: 20,
-                    height: 1.5,
-                    decoration: BoxDecoration(
-                      color: Colors.black,
-                      borderRadius: BorderRadius.circular(2),
-                    ),
-                  ),
-                  Container(
-                    width: 60,
-                    height: 30,
-                    margin: const EdgeInsets.only(left: 25, top: 10, right: 0),
-                    child: TextField(
-                      controller: thirdController,
-                      focusNode: thirdFocusNode,
-                      autofocus: false,
-                      style: const TextStyle(
-                        fontSize: 14,
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
-                      decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "T0023",
-                        hintStyle: TextStyle(
-                          color: Colors.grey[350],
-                          fontSize: 14,
-                          fontWeight: FontWeight.w500,
-                        ),
-                      ),
-                      inputFormatters: <TextInputFormatter>[
-                        UpperCaseTextFormatter(),
-                        LengthLimitingTextInputFormatter(5) //限制长度
-                      ],
-                    ),
-                  ),
+                  textEditingWidget("TACW22", 5, 10, firstController,
+                      firstFocusNode, 6, 7, secondFocusNode),
+                  lineWidget(16, 5, Colors.black, 20, 1.5),
+                  textEditingWidget("40320", 32, 10, secondController,
+                      secondFocusNode, 5, 6, thirdFocusNode),
+                  lineWidget(6, 5, Colors.black, 20, 1.5),
+                  textEditingWidget("T0023", 26, 10, thirdController,
+                      thirdFocusNode, 5, 6, thirdFocusNode),
                 ],
               ),
             ),
-            Container(
-              margin: const EdgeInsets.only(left: 20, top: 5),
-              width: MediaQuery.of(context).size.width - 44,
-              height: 0.5,
-              decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(2),
-              ),
-            ),
+            lineWidget(20, 5, Colors.grey,
+                MediaQuery.of(context).size.width - 44, 0.5),
+            saveWidget(),
           ],
         );
       }),
@@ -216,10 +265,7 @@ class UpperCaseTextFormatter extends TextInputFormatter {
   @override
   TextEditingValue formatEditUpdate(
       TextEditingValue oldValue, TextEditingValue newValue) {
-    /// 判断当前输入框是否存在未完成的字符串
     if (newValue.isComposingRangeValid) return newValue;
-
-    /// ①
     return TextEditingValue(
       text: newValue.text.toUpperCase(),
       selection: newValue.selection,
