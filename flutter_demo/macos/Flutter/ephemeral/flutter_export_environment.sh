14ad028ca3b6b1dbdaa3bc1c83a3de10c92c4b5b
NingSpeals <shuning.wang@chargedot.com>
2022-06-16 09:30:05 +0800

    更新flutter SDK

---
+++ flutter_demo/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/ningspeals/Downloads/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/ningspeals/FlutterProject/flutter_demo"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=/Users/ningspeals/FlutterProject/flutter_demo/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+export "DART_OBFUSCATION=false"
+export "EXTRA_FRONT_END_OPTIONS=--no-sound-null-safety"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/ningspeals/FlutterProject/flutter_demo/.dart_tool/package_config.json"
